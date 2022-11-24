f4114a4ddc4766fb653b282134665b603fdfaa96
unknown <omatix2003@interia.pl>
2022-11-24 21:54:33 +0100

    made base conv init

--- chat/conv.py
+++ chat/conv.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import abort
 
 from chat.auth import login_required
 from chat.db import get_db
+import json
 
 bp = Blueprint('conv', __name__)
 
@@ -20,7 +21,11 @@ def index():
         ).fetchall()
     except Exception:
         pass
-    print(conversations)
+    conversations = [{k: item[k] for k in item.keys()} for item in conversations]
+    for i in conversations:
+        i['users'] = (i['users'].replace("'", "").strip("[]")).split(",")
+    
+    #print((d[0]['users'].replace("'", "").strip("[]")).split(",")[1])
         
     return render_template('chat/index.html', conversations=conversations)
 
@@ -55,3 +60,36 @@ def create():
 
     
     return render_template('chat/create.html')
+
+@bp.route('/conv/<id>', methods=('GET', 'POST'))
+@login_required
+def conversation(id):
+    
+    if request.method == 'POST':
+        nickname = request.form['nickname']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE nickname = ?', (nickname,)
+        ).fetchone()
+
+        if user is None or user['id'] == g.user['id']:
+            error = 'Incorrect nickname.'
+        
+        if error is None:
+            u = [g.user['nickname'], user['nickname']]
+            u = f'\"{u}\"'
+            print(f'\"{u}\"')
+
+            db.execute(
+                f'INSERT INTO conversation (users) VALUES ( {u} )'
+            )
+            db.commit()
+
+            return redirect(url_for('conv.index'))
+        
+        flash(error)
+    print(id)
+    
+
+    return render_template('chat/conversation.html', messages = [])
